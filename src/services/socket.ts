7506c02c7265f192f01b3e72b444f34314878c85
Wesleyss071299 <wesleysilvadesousa@gmail.com>
2024-04-19 18:04:52 -0300

    fix: wss

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -2,4 +2,4 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("ws://moola-api-af585bc6e073.herokuapp.com/");
+export const socket = io("wss://moola-api-af585bc6e073.herokuapp.com/");
