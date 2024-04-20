ea388786a196cbda1557240eee58ce04c24c1d91
Wesleyss071299 <wesleysilvadesousa@gmail.com>
2024-04-20 14:10:07 -0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between h-screen">
       <Navbar />
       {connected ? (
-        <MapComponent />
+        <></>
+        // <MapComponent />
       ) : (
         <div
           className={`${
